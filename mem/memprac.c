2daa16f05686b7f6f956cd1e62680eec53c228c2
Jeff Hammond <hammondj1011@gmail.com>
2025-12-30 20:27:26 -0600

    added some refinements to test more memory

--- mem/memprac.c
+++ mem/memprac.c
@@ -1,12 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct alpha{
+	int a;
+	int b;
+	char *name;
+};
+
+void printHello(){
+	printf("Hello, World!!\n");
+}
 
 int main(){
 
 	int i;
-	for(i=0;i<9;i++)
-		printf("Hello, world!\n");
+	for(i=0;i<9;i++){
+		printHello();
+		struct alpha *holder = malloc(sizeof(holder));
+		holder->a = i+1;
+		holder->b = i+2;
+		holder->name = malloc(sizeof(char)*100);
+		holder->name = "Jason";
+		printf("Nums: %d and %d with %s\n",holder->a,holder->b,holder->name);
+
+		free(holder);
+		holder = NULL;
+	}
 
 	return 0;
 }
