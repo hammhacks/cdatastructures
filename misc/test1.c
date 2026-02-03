4f89a933b976d72174c590069c762e72adc1acd7
Jeff Hammond <hammondj1011@gmail.com>
2026-02-02 19:34:22 -0600

    simple struct refresher

--- misc/test1.c
+++ misc/test1.c
@@ -2,21 +2,37 @@
 #include<stdlib.h>
 #include<string.h>
 
+struct alpha{
+	int a;
+	int b;
+	struct alpha *n;
+}alpha;
+
 int main(int argc, char **argv){
 	char *name = "Jeff";
+	char *xyz = malloc(sizeof(char*)*25);
+	xyz = "Jeff2";
 
-	char *name2 = malloc(sizeof(char*)*5);
-
-	strncpy(name2,name,5);
-	printf("Name is: %s\n",name2);
-	printf("Name flow: %s\n",name2[0]);
-
-	int *x;
-	x = malloc(sizeof(int*));
-	*x=7;
-
-	printf("x is: %d\n",*x);
+	printf("*name is: %s\n",name);
+	printf("*xyz is: %s\n",xyz);
 
 
+	struct alpha *one = malloc(sizeof(struct alpha*));
+
+	one->a=5;
+	one->b=6;
+	one->n = malloc(sizeof(struct alpha*));
+
+	one->n->a=7;
+	one->n->b=8;
+
+	printf("one a is: %d\n",one->a);
+	printf("one b is: %d\n",one->b);
+	
+	struct alpha *two = one->n;
+	
+	printf("two a is: %d\n",two->a);
+	printf("two b is: %d\n",two->b);
+	
 	return 0;
 }
